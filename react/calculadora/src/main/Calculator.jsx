db3c13289f309664fe2a3e1956d62302fca2503c
Ramon Diego <ramondiego857@gmail.com>
2021-08-10 22:43:21 -0300

    Ultima etapa
    Nós terminamos a lógica da calcu colocando o uso das operações na função  setOperation

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -30,7 +30,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]}${currentOperation} ${values[1]}`)
+
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation:equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
 
     }
 
@@ -58,7 +80,7 @@ export default class Calculator extends Component {
         return(
             <div className="calculator">
                 <Display value={this.state.displayValue}/>
-                <Button label="AC" click={ this.clearMemory()} triple/>
+                <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/"  click={this.setOperation} operation/>
                 <Button label="7"  click={this.addDigit}/>
                 <Button label="8"  click={this.addDigit}/>
