dfe2a304a07dc0405132f012dc05fa571db66ecc
Ramon Diego <ramondiego857@gmail.com>
2021-09-02 23:15:04 -0300

    Incorporando "HTML"
    posso colocar arquivos "html" dentro do return da minha função padrão(deafult) no meu arquivo jsx, permitindo que arquivos estáticos(html,css e js) sejam criados pelo lado backend

--- nextJs/fundamentos/src/pages/index.jsx
+++ nextJs/fundamentos/src/pages/index.jsx
@@ -1,3 +1,9 @@
 export default function Inicio() {
-    return  "Início"
+    //"<h1>Título</h1>"
+    return (
+    <div>
+        <h1>Fundamentos de next.js & React</h1>
+        <h2>Vamos estudar esse framework</h2>
+    </div>
+    )
 }
