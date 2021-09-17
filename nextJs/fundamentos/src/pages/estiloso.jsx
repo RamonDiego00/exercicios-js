11344d680fbfa60a37cca845f9e627ccfa97a978
Ramon Diego <ramondiego857@gmail.com>
2021-09-16 22:52:59 -0300

    Layout e {props.children}
    Nesta aula vimos esse conceito importante que é props.children, que graças a ele podemos colocar elementos dentro da tag Layout que nós criamos e então estilizar bnormalmente.

--- nextJs/fundamentos/src/pages/estiloso.jsx
+++ nextJs/fundamentos/src/pages/estiloso.jsx
@@ -1,13 +1,13 @@
 import styles from '../styles/Estiloso.module.css'
+import Layout from '../components/Layout'
 import Link from 'next/link'
 
 export default function Estiloso() {
     return(
+        <Layout titulo="Exemplo de CSS Modularizado">
         <div className={styles.roxo}>
-            <Link href="/">
-            Voltar
-            </Link>
             <h1>Estilo usando Módulos</h1>
         </div>
+        </Layout>
     )
 }
