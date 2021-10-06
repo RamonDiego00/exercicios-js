f6e6a2d8350b3be00c8fa1d8ed1d771218d7949c
Ramon Diego <ramondiego857@gmail.com>
2021-10-05 22:26:48 -0300

    Filtrando dados
    Aprendi a usar o select from, as, where, order e desc nesta aula, que são "parametros" para filtrar uma tabela e adquirir as linhas desejadas

--- banco-relacional/consultarEstados.sql
+++ banco-relacional/consultarEstados.sql
@@ -1,1 +1,14 @@
-SELECT ${*} FROM `table` ${as nameAlias} ${WHERE condition}
+-- Pesquisar toda a tabela(Ctrl+Shift+E)
+SELECT * FROM estados 
+
+-- Pesquisar colunas e nomealas
+SELECT 
+    sigla,
+    nome as 'Nome do Estado'
+     from estados
+     where regiao = 'Sul'
+-- Filtros para encontrar linhas e ordenalas
+SELECT nome, regiao from estados 
+where populacao >= 10 
+order by populacao desc
+-- O select separa as sentenças
