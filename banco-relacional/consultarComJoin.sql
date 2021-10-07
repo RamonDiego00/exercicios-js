4140292174c1a4ee879e2c5725f0c1816eb0786b
Ramon Diego <ramondiego857@gmail.com>
2021-10-06 22:00:22 -0300

    Join ou Where
    Prefiro usar o where, porem o join é o mais correto para fazer essa ligação

--- banco-relacional/consultarComJoin.sql
+++ banco-relacional/consultarComJoin.sql
@@ -3,4 +3,12 @@ select
     c.nome as Cidade, regiao as Região 
 from estados e, cidades c
 where e.id = c.estado_id;
+
+select
+    c.nome as cidade
+    e.nome as estado
+    regiao as região
+from estados e
+inner join cidades c on e.id = c.estado_id
 -- O where esta fazendo o relacionamento
+-- O inner join esta fazendo o relacionamento tbm
