ebcf71ab57c2beb4a9f144458e796bf56e2aa2bd
Ramon Diego <ramondiego857@gmail.com>
2021-10-10 12:28:50 -0300

    Criando e inserindo a tabela Prefeitos
    Nessas 3 aulas vimos o conceitos de inner(relação) outer(externo) entre as tabelas, assim como o conceito de left join, right join e full join.Por ultimo deixamos a FK sendo  id de cidades, porem  a relação  é de um para um, então a FK pode ser nula e deve ser unica,já que nem todos devem se relacionar

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,15 @@
+select * from cidades;
+
+insert into prefeitos
+    (nome, cidade_id )
+values
+    ('Rodrigo Neves', 5),
+    ('Raquel Lira', 6),
+    ('Zenaldo Coutinho', null);
+
+    select * from prefeitos;
+
+insert into prefeitos
+    (nome, cidade_id )
+values
+    ('Rafael Grega', null);
