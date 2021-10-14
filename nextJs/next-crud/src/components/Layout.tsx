54fa384d3b6f98e419068090cb5a6579867b7d95
Ramon Diego <ramondiego857@gmail.com>
2021-10-13 23:28:29 -0300

    Metade da aula de Components
    Aula meio confusa, com relação aos props e a forma que o layout vai pegar a função titulo e modificar nela mesma, espero que faça sentido.

---
+++ nextJs/next-crud/src/components/Layout.tsx
@@ -0,0 +1,14 @@
+export default function Layout(props) {
+    return(
+        <div className={`
+            flex flex-col w-2/3
+            bg-white text-gray-800
+        `}>
+            <Titulo></Titulo>
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
---
+++ nextJs/next-crud/src/components/Titulo.tsx
@@ -0,0 +1,8 @@
+exports default function Titulo(props) {
+    return (
+        <div>
+            <h1>{props.children}</h1>
+            <hr />
+        </div>
+    )
+}
