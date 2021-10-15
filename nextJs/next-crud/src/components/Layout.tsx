08ceee280a943167a866a6e1d71e0aeae781c253
Ramon Diego <ramondiego857@gmail.com>
2021-10-15 20:52:20 -0300

    interface e LayoutProps
    Essa aula ficou bem confuda esse conceito do children, filho de um componente funcional vai herdar e ser editado de forma dinâmica, mas basicamente ele criou uma função que vai receber dois métodos padrões que podem ser alterados quando ela é chamada(Layout.tsx)

--- nextJs/next-crud/src/components/Layout.tsx
+++ nextJs/next-crud/src/components/Layout.tsx
@@ -1,10 +1,20 @@
-export default function Layout(props) {
+import Titulo from "./Titulo"
+
+interface LayoutProps {
+    titulo?: string
+    children: any
+}
+
+export default function Layout(props: LayoutProps) {
     return(
         <div className={`
             flex flex-col w-2/3
-            bg-white text-gray-800
+            bg-white text-gray-800 rounded-md
         `}>
-            <Titulo></Titulo>
+            <Titulo>{props.titulo}</Titulo>
+            <div className="p-6">
+                {props.children}
+            </div>
             <div>
 
             </div>
--- nextJs/next-crud/src/pages/index.tsx
+++ nextJs/next-crud/src/pages/index.tsx
@@ -1,11 +1,16 @@
+import Layout from "../components/Layout"
+
 export default function Home() {
   return (
     <div className={`
       flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500
     text-white
+
     `}>
-      <h1>App</h1>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
