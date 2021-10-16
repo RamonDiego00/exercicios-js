5cd76cff38e53b314d46c6c2632647a90a41aeae
Ramon Diego <ramondiego857@gmail.com>
2021-10-16 14:54:41 -0300

    Componente tabela e estilização
    Criamos o componente tabela que em sua estrutura te duas funções que são retornadas no return dela dentro de uma tag table, porem é bem confuso entender porque na verdade os dados estão dentro de um array do index.tsx .Depois usamos o tailwind e usamos o className para estilizar as linhas e colunas da tabela

--- nextJs/next-crud/src/pages/index.tsx
+++ nextJs/next-crud/src/pages/index.tsx
@@ -1,6 +1,14 @@
 import Layout from "../components/Layout"
-
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -9,7 +17,9 @@ export default function Home() {
 
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}>
+
+        </Tabela>
       </Layout>
     </div>
   )
