8cd490f2039c6df305f06af68e116dfe25009899
Ramon Diego <ramondiego857@gmail.com>
2021-10-17 17:40:06 -0300

    Conclusão do módulo
    Fooi bem dificil integrar essa parte final do formulário com a parte do firebase, porém deu tudo certo e a integração com o repositório deu certo e os dados são armazenados e tratados

--- nextJs/next-crud/src/core/ClienteRepositorio.ts
+++ nextJs/next-crud/src/core/ClienteRepositorio.ts
@@ -2,6 +2,6 @@ import Cliente from "./Cliente";
 
 export default interface ClienteRepositorio {
     salvar(cliente: Cliente) : Promise<Cliente>
-    excluir(cliente: Cliente) : Promise<Cliente>
-    obtertodos() : Promise<Cliente[]>
+    excluir(cliente: Cliente) : Promise<void>
+    obterTodos() : Promise<Cliente[]>
 }
