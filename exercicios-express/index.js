5478f6121fb39adfe7bc8ed9b60991e739beb1c3
Ramon Diego dos Santos Ferreira <ramondiego857@gmail.com>
2022-02-05 17:00:50 -0300

    Middleware

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,40 @@
  const exprees = require('express')
  const app = exprees()
 
+ app.use('/opa',(req, res, next)=> {
+    console.log('Antes...')
+    next()
+})
+
+ app.get('/opa',(req, res, next) => {
+     console.log('Durante...')
+    res.json({
+       data: [ {id:7, name:'Ramon', position:1},
+        {id:34, name:'Bia', position:2},
+        {id:73, name:'Carlos', position:3}
+       ],
+       count:30,
+       skip:0,
+       limit:3,
+       status:200
+    })
+    next()
+
+    //  res.json({
+    //     name: 'Cellphone',
+    //     price: 1899.00,
+    //     discount: 0.12
+    //  })
+
+
+    //  res.send('<h1>Testando</h1>')
+ })
+
+ 
+app.use('/opa',(req, res)=> {
+    console.log('Depois...')
+})
+
  app.listen(3000, ()=> {
      console.log("Backend Rodando...")
  })
